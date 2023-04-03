f91a70e341a612e97b8193600fa3d6ab7b26c55f
muhammadnoumanpervaiz <nomanpervaiz42@gmail.com>
2023-04-03 10:48:43 -0700

    added on first welcoming page

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -3,20 +3,16 @@ import {Grid, Button, Typography} from '@mui/material';
 
 const Auth = () => {
   return (
-    <Grid container sx={{display:"flex", flexDirection:"column",justifyContent:"center", alignItems:"center", height:"100vh",}}>
-        <Grid item xs={6} sx={{display:"flex",borderRadius:"10px" ,boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;", rowGap:"30px", flexDirection:"column",justifyContent:"center", width:"100%"}}>
-            <Grid >
-                <Button href='/auth-signup' sx={{width:"50%"}} variant="contained">Signup</Button>
-            </Grid>
-            <Grid>
-                <Typography sx={{fontSize:"12px", mb:1, color:"blue"}}>
-                Do you have Already Account?
-                </Typography>
-                <Button href='/auth-login' sx={{width:"50%"}} variant="outlined">
-                Login
-                </Button>
-            </Grid>
-        </Grid>
+    <Grid container sx={{display:"flex",mt:10,rowGap:"20px", width:"50%", 
+      flexDirection:"column",justifyContent:"center", alignItems:"center", 
+      height:"60vh",boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;"}}>
+        <Button href='/auth-signup' sx={{width:"50%"}} variant="contained">Signup</Button>
+        <Typography sx={{fontSize:"12px", mb:1, color:"blue"}}>
+        Do you have Already Account?
+        </Typography>
+        <Button href='/auth-login' sx={{width:"50%"}} variant="outlined">
+        Login
+        </Button>
     </Grid>
   )
 }
